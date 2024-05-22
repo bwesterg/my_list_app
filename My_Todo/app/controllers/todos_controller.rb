805dbf876bec746ba9431371c16e8f5719e940e9
bwesterg <benton.westergaard@gmail.com>
2024-05-22 17:06:42 -0600

    adds edit function and begins to implement conditional rendering to update todos.

--- My_Todo/app/controllers/todos_controller.rb
+++ My_Todo/app/controllers/todos_controller.rb
@@ -1,24 +1,43 @@
 class TodosController < ApplicationController
-
+  before_action :find_todo, only: [:update, :destroy]
   def index
     @todos = Todo.all
     render json: @todos
   end
 
   def create
-    @todo = Todo.create(
-      name: params[:name],
-      description: params[:description],
-      urgency: params[:urgency],
-      urgent: params[:urgent],
-      done: params[:done]
-    )
+    # @todo = Todo.create(
+    #   name: params[:name],
+    #   description: params[:description],
+    #   urgency: params[:urgency],
+    #   urgent: params[:urgent],
+    #   done: params[:done]
+    # )
+    @todo = Todo.create(todo_params)
+
     render json: @todo, status: :created
   end
 
+  def update
+    # @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    render json: @todo
+  end
+
   def destroy
-    @todo = Todo.find(params[:id])
+    # @todo = Todo.find(params[:id])
     @todo.destroy
     render status: :no_content
-  end 
+  end
+  
+
+  private
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :description, :urgency, :urgent, :done)
+  end
 end
